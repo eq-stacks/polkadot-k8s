298b35c447d906f38d671601c22d16a1140e5af6
Mark Henderson <mark@mrh.io>
2022-03-02 15:09:09 -0500

    feat: subnetwork and k8s cluster

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,7 @@ variable "org_id" {}
 variable "billing_account" {}
 variable "project_owners" {}
 variable "project_host_vpc" {}
+variable "subnetwork_id" {}
 
 terraform {
   required_providers {
@@ -46,4 +47,89 @@ resource "google_project_iam_binding" "target_project_owners" {
 resource "google_compute_shared_vpc_service_project" "service_project" {
   host_project    = var.project_host_vpc
   service_project = google_project.target_project.project_id
+}
+
+resource "google_project_service" "container" {
+  project = google_project.target_project.id
+  service = "container.googleapis.com"
+
+  disable_dependent_services = true
+  depends_on = [
+    google_project.target_project
+  ]
+}
+
+resource "google_project_iam_binding" "host_project" {
+  project = var.project_host_vpc
+  role    = "roles/container.hostServiceAgentUser"
+  members = [
+    "serviceAccount:service-${google_project.target_project.number}@container-engine-robot.iam.gserviceaccount.com"
+  ]
+  depends_on = [
+    google_project_service.container
+  ]
+}
+
+data "google_compute_subnetwork" "primary" {
+  name    = var.subnetwork_id
+  region  = "us-east4"
+  project = var.project_host_vpc
+}
+
+resource "google_compute_subnetwork_iam_binding" "subnetwork" {
+  project    = var.project_host_vpc
+  region     = "us-east4"
+  subnetwork = data.google_compute_subnetwork.primary.id
+  role       = "roles/compute.networkUser"
+  members = [
+    "serviceAccount:service-${google_project.target_project.number}@container-engine-robot.iam.gserviceaccount.com",
+    "serviceAccount:${google_project.target_project.number}@cloudservices.gserviceaccount.com"
+  ]
+}
+
+data "google_compute_network" "host_vpc" {
+  name    = "host-vpc"
+  project = var.project_host_vpc
+}
+
+resource "google_container_cluster" "testnet" {
+  name     = "testnet"
+  location = "us-east4-c"
+  project  = google_project.target_project.project_id
+
+  # Create the smallest possible default node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = data.google_compute_network.host_vpc.self_link
+  subnetwork = data.google_compute_subnetwork.primary.self_link
+  # network = "projects/host-vpc-project-10/global/networks/host-vpc"
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "${data.google_compute_subnetwork.primary.name}-pods"
+    services_secondary_range_name = "${data.google_compute_subnetwork.primary.name}-services"
+  }
+
+  depends_on = [
+    google_project_service.container,
+    google_project_iam_binding.host_project,
+    google_compute_subnetwork_iam_binding.subnetwork
+  ]
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "primary"
+  cluster    = google_container_cluster.testnet.id
+  node_count = 3
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-standard-4"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    # service_account = google_service_account.default.email
+    # oauth_scopes = [
+    #   "https://www.googleapis.com/auth/cloud-platform"
+    # ]
+  }
 }
